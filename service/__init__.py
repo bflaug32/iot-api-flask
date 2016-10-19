037f08c06399f5edab923c4f6e8b8056bd66760b
Brinn Riordan <brinn.riordan@gmail.com>
2016-10-19 19:27:45 -0400

    update config_local

--- service/__init__.py
+++ service/__init__.py
@@ -7,13 +7,7 @@ app = Flask(__name__)
 app.config.from_object('config_local.Config')
 
 # add caches to save data about the state of each object
-if app.config['DEBUG'] is True:
-    import fakeredis
-    commands = fakeredis.FakeStrictRedis(0)
-else:
-    import redis
-    _redis_host = app.config['REDIS_HOST']
-    _redis_port = app.config['REDIS_PORT']
-    commands = redis.StrictRedis(host=_redis_host, port=_redis_port, db=0)
+import fakeredis
+commands = fakeredis.FakeStrictRedis(0)
 
 from service import views
